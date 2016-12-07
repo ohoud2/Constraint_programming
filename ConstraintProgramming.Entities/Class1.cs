cc3c0a248e22e713cdd2897573e9c268c38da46e
Zaid <e0013259@u.nus.edu>
2016-12-07 21:06:48 +0800

    Delete FrontEnd Web

---
+++ ConstraintProgramming.Entities/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.SolverFoundation.Common;
+using Microsoft.SolverFoundation.Solvers;
+using Microsoft.SolverFoundation.Services;
+
+namespace ConstraintProgramming.Entities {
+    public class Class1 {
+        public Class1() {
+
+            SolverContext _context = SolverContext.GetContext();
+            Model _model = _context.CreateModel();
+            _model.Ad
+        }
+    }
+}
--- ConstraintProgramming.FrontEnd/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConstraintProgramming.FrontEnd.MvcApplication" Language="C#" %>
